3bffd40c48cb7d8fba7976263e8735ce0416657a
张贞翔 <zhangzhenxiang2004@outlook.com>
2025-09-01 17:11:31 +0800

    style(utils): 调整Apply的格式

--- src/utils/traits.rs
+++ src/utils/traits.rs
@@ -1,32 +1,33 @@
 use async_trait::async_trait;
 use futures::{Future, StreamExt, future::BoxFuture};
 
-use std::pin::Pin;
 use tokio_stream::wrappers::ReceiverStream;
 
 #[async_trait]
 pub trait AsyncFrom<T> {
     async fn async_from(value: T) -> Self;
 }
-
 pub trait Apply<T> {
-    fn apply_async<F>(self, call: F) -> impl Future<Output = ()>
+    fn apply_async<F, Fut>(self, call: F) -> impl Future<Output = ()>
     where
-        F: Fn(T) -> Pin<Box<dyn Future<Output = ()>>>;
+        F: Fn(T) -> Fut,
+        Fut: Future<Output = ()> + Send + 'static;
 
     fn apply_with_capture_async<C, F>(self, capture: C, call: F) -> impl Future<Output = C>
     where
         F: for<'a> Fn(&'a mut C, T) -> BoxFuture<'a, ()>;
 
-    fn fold_async<F, C>(self, capture: C, call: F) -> impl Future<Output = C>
+    fn fold_async<F, C, Fut>(self, capture: C, call: F) -> impl Future<Output = C>
     where
-        F: Fn(C, T) -> Pin<Box<dyn Future<Output = C>>>;
+        F: Fn(C, T) -> Fut,
+        Fut: Future<Output = C> + Send + 'static;
 }
 
 impl<T> Apply<T> for ReceiverStream<T> {
-    async fn apply_async<F>(mut self, call: F)
+    async fn apply_async<F, Fut>(mut self, call: F)
     where
-        F: Fn(T) -> Pin<Box<dyn Future<Output = ()>>>,
+        F: Fn(T) -> Fut,
+        Fut: Future + Send + 'static,
     {
         while let Some(result) = self.next().await {
             call(result).await;
@@ -43,9 +44,10 @@ impl<T> Apply<T> for ReceiverStream<T> {
         capture
     }
 
-    async fn fold_async<F, C>(mut self, mut capture: C, call: F) -> C
+    async fn fold_async<F, C, Fut>(mut self, mut capture: C, call: F) -> C
     where
-        F: Fn(C, T) -> Pin<Box<dyn Future<Output = C>>>,
+        F: Fn(C, T) -> Fut,
+        Fut: Future<Output = C> + Send + 'static,
     {
         while let Some(result) = self.next().await {
             capture = call(capture, result).await;
